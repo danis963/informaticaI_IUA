4c44d662cac9fac88e847259dc6d7df33ace50f6
Daniel Rosso <daniel.rosso@i-terasic.com>
2021-03-26 20:35:16 -0300

    edad peso

---
+++ c/src/1-0_data_types_edad_peso.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int edad=0;
+  float peso=0;
+  printf("Ingrese su edad \n");
+  scanf("%d",&edad);
+  printf("Ingrese su peso\n");
+  scanf("%f",&peso);
+  printf("Su edad es %d y su peso %f\n",edad,peso);
+}
