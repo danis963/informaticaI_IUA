815156a3ed08fe7a9498b79042408aacedfe962c
Daniel Rosso <daniel.rosso@i-terasic.com>
2021-05-18 17:05:43 -0300

    array int index

--- c/arrays/1-array_example.c
+++ c/arrays/1-array_example.c
@@ -3,4 +3,8 @@ temp[1]=50.29;
 temp[2]=10.22;
 temp[3]=-10.21;
 temp[4]=35.00;
+
+int indice=4;
+hum[4]=10.89;
 hum[2+2]=10.89;
+hum[indice]=10.89;
--- c/arrays/1-problem_without_arrays.c
+++ c/arrays/1-problem_without_arrays.c
@@ -11,7 +11,7 @@ int menu (void)
 	float temp16=0; float temp18=0;
 	float temp17=0; float temp20=0;
 	float temp19=0; float temp22=0;
-	float temp23=0;
+	float temp23=0; float temp24=0;
 	float hum1=0; 	float hum2=0;
 	float hum3=0; 	float hum4=0;
 	//...//
